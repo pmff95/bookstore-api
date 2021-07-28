1b59fba9135b0459ceb2a737d1e658855ea973b6
pmff95 <paulomesquita0@gmail.com>
2021-07-28 08:52:39 -0300

    commit inicial

---
+++ src/test/java/com/teste/springCurso/SpringCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.springCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
